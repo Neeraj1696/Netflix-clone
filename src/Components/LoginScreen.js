5dd345abdc3f9a7c7aac5fdb114795682e6b54d3
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-04-14 00:56:11 +0530

    initial commit

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignInScreen from "./SignInScreen";
 function LoginScreen() {
+  const [signIn, setSignin] = useState(false);
   return (
     <div className="LoginScreen">
       <div className="loginscreen_background">
@@ -10,8 +12,38 @@ function LoginScreen() {
           alt="ERROR image"
         />
 
-        <button className="loginScreen_button">Sign in</button>
-        <div className="loginScreen_gradient"></div>
+        <button onClick={() => setSignin(true)} className="loginScreen_button">
+          Sign in
+        </button>
+        <div className="loginScreen_gradient">
+          <img
+            className="loginScreen_image"
+            src="https://images.ctfassets.net/7so8go2zrvbw/2cPBaJIBwmXKiPFmORaRlw/1288322d48e889c11a40b615899230ee/0af6ce3e-b27a-4722-a5f0-e32af4df3045_what_is_netflix_5_en.png?w=800&h=450&q=95&fm=png"
+            alt="ERROR-background"
+          />
+        </div>
+        <div className="loginScreen_body">
+          {signIn ? (
+            <SignInScreen />
+          ) : (
+            <>
+              <h4>Welcome back!</h4>
+              <h1>Unlimited movies, TV shows and more.</h1>
+              <h2>Watch anywhere. Cancel anytime.</h2>
+              <div className="loginScreen_input">
+                <form>
+                  <input type="email" placeholder="Email Address" />
+                  <button
+                    onClick={() => setSignin(true)}
+                    className="loginScreen_getStarted"
+                  >
+                    GET STARTED
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/SignInScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./SignInScreen.css";
+
+function SignInScreen() {
+  return (
+    <div className="SignInScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="SignInScreen_grey">New to Netflix?</span>
+          Sign up now.
+        </h4>
+      </form>
+    </div>
+  );
+}
+
+export default SignInScreen;
