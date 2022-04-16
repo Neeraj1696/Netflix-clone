9ebc11ef88dfd15784419addde4d4a93eaaca32e
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-04-16 23:04:42 +0530

    sign in commit

--- src/Components/SignInScreen.js
+++ src/Components/SignInScreen.js
@@ -2,17 +2,31 @@ import React from "react";
 import "./SignInScreen.css";
 
 function SignInScreen() {
+  const register = (e) => {
+    e.preventDefault();
+    // console.log("Register");
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    // console.log("signIn");
+  };
+
   return (
     <div className="SignInScreen">
       <form>
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           <span className="SignInScreen_grey">New to Netflix?</span>
 
-          <span className="SignInScreen_link">Sign up now.</span>
+          <span className="SignInScreen_link" onClick={register}>
+            Sign up now.
+          </span>
         </h4>
       </form>
     </div>
